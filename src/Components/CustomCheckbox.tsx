6c2e4510420d915533dcfbb2e94bc0cbe308f41e
rostredko <rost.redko@gmail.com>
2025-07-09 02:43:01 +0300

    Code fixes that breaks build

--- src/Components/CustomCheckbox.tsx
+++ src/Components/CustomCheckbox.tsx
@@ -1,7 +1,7 @@
 import { Checkbox } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+const CustomCheckbox = styled(Checkbox)(() => ({
   paddingRight: '15px',
   '& svg': {
     fontSize: '2.5rem',
